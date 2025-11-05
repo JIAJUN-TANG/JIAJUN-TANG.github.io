9a35e40c7200ba336336b5e0a21c992344d8aeda
Jiajun Tang <zhiju022486@163.com>
2025-11-05 21:17:11 +0800

    Reapply "	new file:   _publications/2025/2025-pub-5.md"
    This reverts commit 8b7711f9e9386e987bf9f8fb97f239a347eea671.

--- get_google_scholar_citations.py
+++ get_google_scholar_citations.py
@@ -23,3 +23,21 @@ scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(json.dumps(author, indent=2))
+
+# 创建results目录
+os.makedirs('results', exist_ok=True)
+
+# 保存完整的作者数据
+with open(f'results/gs_data.json', 'w', encoding='utf-8') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
+
+# 创建shields.io格式的数据
+shieldio_data = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['citedby']}",
+}
+
+with open(f'results/gs_data_shieldsio.json', 'w', encoding='utf-8') as outfile:
+    json.dump(shieldio_data, outfile, ensure_ascii=False)
