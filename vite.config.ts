f57bc93e31f5a482edfad58558957ba61f3f23af
Jiajun Tang <zhiju022486@163.com>
2026-03-11 14:03:49 +0800

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     // Base path for GitHub Pages
-    base: '/JIAJUNTANG.github.io/',
+    base: './',
     build: {
       outDir: 'dist',
       emptyOutDir: true,
